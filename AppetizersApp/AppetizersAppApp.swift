a3a381566e960d0c7d4060df02e14af8374a7510
Mahmoud-Aziz <mahmoudmohammedaziz@gmail.com>
2023-08-26 14:39:19 +0300

    Initial commit

---
+++ AppetizersApp/AppetizersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersAppApp.swift
+//  AppetizersApp
+//
+//  Created by Mahmoud Aziz on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
